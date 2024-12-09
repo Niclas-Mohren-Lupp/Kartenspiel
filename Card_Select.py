91307dbafffd3d3ed9a2b0b9cba3d6a0eed8a59e
Niclas Mohren <Niclas.Mohren@lupp.de>
2024-12-09 10:16:28 +0100

    Algorythmus Karte ziehen

--- Card_Select.py
+++ Card_Select.py
@@ -1,36 +1,42 @@
 import random
+import math
 Max_Karten = 51
 Zeiten_dict = {i: 0 for i in range(1, Max_Karten + 1)}
 
-def berechne_Wahrscheinlichkeit(A):
-    print("Hallo")
-    return 
+def berechne_Wahrscheinlichkeit(zeiten_dict):
+    #ChatGPT
+    k = 0.1
 
-def Wahrscheinlichkeitsalgorythmus():
-    List_Karten = list(range(1, Max_Karten + 1))
-    Wahrscheinlichkeit = berechne_Wahrscheinlichkeit(Zeiten_dict)
-    summe = sum(Wahrscheinlichkeit)
-    Karte = random.choices(List_Karten, Wahrscheinlichkeit, k=1) [0]
-    return Karte
+# Schritt 1: Berechne die Gewichtungen (w(x) = e^(k * Zeiten_dict[key]))
+    Gewichtungen = {key: math.exp(k * value) for key, value in Zeiten_dict.items()}
 
-def Karteziehen():
-    Random_Karte = Wahrscheinlichkeitsalgorythmus()
+# Schritt 2: Berechne die Summe der Gewichtungen
+    summe_gewichtungen = sum(Gewichtungen.values())
+
+# Schritt 3: Normalisiere die Gewichtungen, um Wahrscheinlichkeiten zu erhalten
+    Wahrscheinlichkeiten = {key: gewicht / summe_gewichtungen for key, gewicht in Gewichtungen.items()}
+
+    return Wahrscheinlichkeiten
+
+def Wahrscheinlichkeitsalgorythmus(zeiten_dict):
+    List_Karten = list(zeiten_dict.keys())
+    Wahrscheinlichkeit = berechne_Wahrscheinlichkeit(zeiten_dict)
+    Random_Karte = random.choices(List_Karten, weights = Wahrscheinlichkeit, k=1) [0]
+    
     return Random_Karte
 
-def hochzealen(i):
-    global Zeiten_dict
-    Nummer = i + 1
-    Zeiten_dict[Nummer] = 1
+def hochzealen(Random_Karte, Zeiten_dict):
+    Zeiten_dict[Random_Karte] = 1
 
     for key in Zeiten_dict:
-        if Zeiten_dict[key] >= 1:
-            Zeiten_dict[key] += 1
+        if key != Random_Karte:
+            if Zeiten_dict[key] < 51:
+                Zeiten_dict[key] += 1
         
-        if Zeiten_dict[key] > 51:
-            Zeiten_dict[key] = 0
+            else:
+                Zeiten_dict[key] = 0
 
 
 for _ in range (1, 1000):
-    Random_Karte = Karteziehen()
-    hochzealen(Random_Karte)
-print(Zeiten_dict)
+    Random_Karte = Wahrscheinlichkeitsalgorythmus(Zeiten_dict)
+    hochzealen(Random_Karte, Zeiten_dict)
